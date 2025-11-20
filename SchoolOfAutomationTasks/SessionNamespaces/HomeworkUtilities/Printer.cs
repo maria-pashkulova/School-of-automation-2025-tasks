bf9873761d11a3bfc81a0cc3b53452d72450e303
Maria Pashkulova <132500391+maria-pashkulova@users.noreply.github.com>
2025-11-20 06:51:04 +0200

    Complete tasks 6 and 7

---
+++ SchoolOfAutomationTasks/SessionNamespaces/HomeworkUtilities/Printer.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+//Create a custom namespace called HomeworkUtilities with a class Printer that prints a message.
+namespace SessionNamespaces.HomeworkUtilities
+{
+   
+    internal class Printer
+    {
+        private static int usageCount = 0;
+        public static void PrintMessage(string message)
+        {
+            usageCount++;
+            Console.WriteLine(message);
+        }
+
+        public static void GetPrintedMessagesCount()
+        {
+            Console.WriteLine($"Printed {usageCount} message(s) by far");
+        }
+    }
+}
--- SchoolOfAutomationTasks/SessionNamespaces/Program.cs
+++ SchoolOfAutomationTasks/SessionNamespaces/Program.cs
@@ -1,10 +1,24 @@
-﻿namespace SessionNamespaces
+﻿using SessionNamespaces.HomeworkUtilities;
+
+
+namespace SessionNamespaces
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            //Declare variables of different types and use keywords like const, var, and static.
+            const string prompt = "Enter a message to print:";
+            var author = "Maria";
+
+            Console.Write(prompt);
+            string userInput = Console.ReadLine();
+            
+            Printer.PrintMessage("You said: " + userInput);
+
+
+            Printer.GetPrintedMessagesCount();
+            
         }
     }
 }
