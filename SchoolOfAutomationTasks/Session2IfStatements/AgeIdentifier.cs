3ceef5d69294f3817e55f9dc994d277e8d20e18c
Maria Pashkulova <mariapashkulova28@gmail.com>
2025-11-23 16:30:14 +0200

    Complete first task

---
+++ SchoolOfAutomationTasks/Session2IfStatements/AgeIdentifier.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Session2IfStatements
+{
+    internal class AgeIdentifier
+    {
+        public static void DefineMinorOrAdult()
+        {
+            byte age;
+
+            while (true)
+            {
+
+                Console.Write("Enter your age: ");
+
+                try
+                {
+                    age = byte.Parse(Console.ReadLine());
+                    break;
+
+                }
+                catch (Exception)
+                {
+                    Console.WriteLine("Invalid age! Please enter a non-negative valid number!");
+                }
+            }
+
+            if (age < 18)
+            {
+                Console.WriteLine("You are a minor.");
+            }
+            else
+            {
+                Console.WriteLine("You are an adult.");
+            }
+        }
+        
+
+
+    }
+}
---
+++ SchoolOfAutomationTasks/Session2IfStatements/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace Session2IfStatements
+
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Task 6: Ask the user for their age and print whether they are a minor or an adult.");
+            AgeIdentifier.DefineMinorOrAdult();
+
+
+        }
+    }
+}
