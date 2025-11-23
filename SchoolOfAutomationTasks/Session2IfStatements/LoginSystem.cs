526258157a45913db5df8fa27fc78f89e0c63e16
Maria Pashkulova <mariapashkulova28@gmail.com>
2025-11-23 18:10:51 +0200

    Complete bonus challange 1

---
+++ SchoolOfAutomationTasks/Session2IfStatements/LoginSystem.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Session2IfStatements
+{
+    static class LoginSystem
+    {
+        const string correctUsername = "mpashkulova";
+        const string correctPassword = "mpass";
+
+        public static void CheckCredentials()
+        {
+           Console.Write("Enter username: ");
+           string inputUsername = Console.ReadLine().Trim();
+           Console.Write("Enter password: ");
+           string inputPassword = Console.ReadLine().Trim();
+
+            if (String.IsNullOrEmpty(inputUsername) || String.IsNullOrEmpty(inputPassword))
+            {
+                Console.WriteLine("Username and password can't be empty");
+                return; //stop further checking if input is empty / null
+            }
+
+            if (inputUsername == correctUsername)
+            {
+                if (inputPassword == correctPassword)
+                {
+                    Console.WriteLine("Correct username and password");
+                }
+                else
+                {
+                    Console.WriteLine("Correct username but wrong password");
+                }
+            }
+            else
+            {
+                Console.WriteLine("Wrong username");
+            }
+
+        }
+
+    }
+}
--- SchoolOfAutomationTasks/Session2IfStatements/Program.cs
+++ SchoolOfAutomationTasks/Session2IfStatements/Program.cs
@@ -10,9 +10,11 @@ namespace Session2IfStatements
             Console.WriteLine("Task 6: Ask the user for their age and print whether they are a minor or an adult.");
             AgeIdentifier.DefineMinorOrAdult();
 
-            Console.WriteLine("Task 7: Ask for a student's grade and print a performance message using nested if statements.");
+            Console.WriteLine("\nTask 7: Ask for a student's grade and print a performance message using nested if statements.");
             SchoolPerformanceChecker.GiveGradeFeedback();
 
+            Console.WriteLine("\nBonus challange 1: Create a login system that checks username and password using nested if statements.Print appropriate messages for: Correct username and password, Correct username but wrong password, Wrong username.");
+            LoginSystem.CheckCredentials();
         }
     }
 }
