98ecfd92bcb8f0f44d3a64b28c16bb346f6297f4
Maria Pashkulova <mariapashkulova28@gmail.com>
2025-11-27 00:07:48 +0200

    Revert "Edit project directory for HW 3"
    This reverts commit c2a936417e9c9a2f2f461b9ac83456185af1ab29.

---
+++ SchoolOfAutomationTasks/BonusChallange/Program.cs
@@ -0,0 +1,46 @@
+﻿namespace BonusChallange
+{
+
+    /*Loop through all numbers between N and 999 from the console, 
+     where N can be between (minus) 1000 and 999, and prints even numbers, skip negatives,
+      and stop at 999. 
+    */
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n;
+
+            while (true)
+            {
+                Console.Write("Enter N (-1000 to 999): ");
+
+                //Validate input: N must be an integer between - 1000 and 999
+
+                if (int.TryParse(Console.ReadLine(), out n) && n >= -1000 && n <= 999)
+                {
+                    break;   // n is valid so we exit the while loop
+                }
+
+                Console.WriteLine("Invalid input! Please enter an integer between -1000 and 999.");
+            }
+
+            Console.WriteLine( $"All positive even numbers between {n} and 999 are:");
+
+            for (int i = n; i <= 999; i++)
+            {
+                if (i < 0)
+                {
+                    continue; //skip negative numbers
+                }
+
+                if (i % 2 == 0)
+                {
+                    Console.WriteLine(i);
+                }
+            }
+
+        }
+
+    }
+}
