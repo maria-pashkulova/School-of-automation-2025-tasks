7402c95467838d859c682fb52419734c10443a1a
Maria Pashkulova <mariapashkulova28@gmail.com>
2025-11-27 06:23:31 +0200

    Complete Task 1

---
+++ SchoolOfAutomationTasks/HW_Session_3_Loops/Task1/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace Task1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Printing numbers from 1 to 10:");
+            for (int i = 1; i <= 10; i++)
+            {
+                Console.WriteLine(i);
+            }
+        }
+    }
+}
