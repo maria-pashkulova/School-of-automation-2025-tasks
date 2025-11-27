fbd3eecb03e3c7d856d77f2283dbecda63fb8194
Maria Pashkulova <mariapashkulova28@gmail.com>
2025-11-27 07:00:02 +0200

    Complete Task 5

---
+++ SchoolOfAutomationTasks/HW_Session_3_Loops/Task5/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace Task5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Printing a multiplication table from 1 to 5 using nested for loops: " + Environment.NewLine);
+            
+            for (int i = 1; i <= 5; i++)
+            {
+                for (int j = 1; j <= 5; j++)
+                {
+                    //{0,-12} left-align in a 12-character field ->
+                    //used to print expression with 12-character spacing between 'columns'
+                    
+                    Console.Write("{0,-12}",$"{i} * {j} = {i * j}");
+                }
+                Console.WriteLine(); // move to the next row after finishingthe inner loop
+            }
+        }
+    }
+}
