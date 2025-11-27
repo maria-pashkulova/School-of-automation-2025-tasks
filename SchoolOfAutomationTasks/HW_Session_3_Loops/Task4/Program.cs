2c6524463d6274d58c83cf9707c54584c304654d
Maria Pashkulova <mariapashkulova28@gmail.com>
2025-11-27 06:40:52 +0200

    Complete Task 4

---
+++ SchoolOfAutomationTasks/HW_Session_3_Loops/Task4/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace Task4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string userInput;
+
+            do
+            {
+                Console.Write("Enter anything to continue, enter N to exit the program: ");
+                userInput = Console.ReadLine().ToUpper();
+            }
+            while (userInput != "N");
+
+            Console.WriteLine("Program ended.");
+        }
+    }
+}
