d34dc9b4caa31272293dc32baa305f9fbb2bff4b
Maria Pashkulova <mariapashkulova28@gmail.com>
2025-11-27 06:36:15 +0200

    Complete task 3

---
+++ SchoolOfAutomationTasks/HW_Session_3_Loops/Task3/Program.cs
@@ -0,0 +1,31 @@
+﻿namespace Task3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double number;
+
+            while (true)
+            {
+                Console.Write("Enter a number greater than 100: ");
+
+                if (double.TryParse(Console.ReadLine(), out number) && number > 100)
+                {
+                    break;   // number is valid so we exit the while loop
+                }
+
+                Console.WriteLine("Invalid input! Please enter an number greater than 100!");
+                
+            }
+
+            //print numbers from 1 to the provided number
+            int i = 1;
+            while (i <= number)
+            {
+                Console.WriteLine(i);
+                i++;
+            }
+        }
+    }
+}
