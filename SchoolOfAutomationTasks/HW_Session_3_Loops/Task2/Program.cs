81d1d6a433a755ce1deb69e3e069e01b0e5466e5
Maria Pashkulova <mariapashkulova28@gmail.com>
2025-11-27 06:27:21 +0200

    Complete Task 2

---
+++ SchoolOfAutomationTasks/HW_Session_3_Loops/Task2/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace Task2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Printing numbers from an array using foreach loop:");
+            byte[] numbers = [1, 2, 3, 4, 5];
+
+            foreach (byte number in numbers) 
+            {
+                Console.WriteLine(number);
+            
+            }
+        }
+    }
+}
