3e9783091024a592a84987028c9c608c630426fe
Maria Pashkulova <132500391+maria-pashkulova@users.noreply.github.com>
2025-11-30 12:09:34 +0200

    Complete Task 2

---
+++ SchoolOfAutomationTasks/HW_Session_4_Methods/FunGreetings/Program.cs
@@ -0,0 +1,30 @@
+﻿using System.Xml.Linq;
+
+namespace FunGreetings
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter your name: ");
+            string userName = Console.ReadLine();
+            Console.WriteLine(MagicGreeting(userName));
+        }
+
+        static string MagicGreeting(string userName)
+        {
+            string[] greetings =
+                {
+                    $"Welcome, mighty {userName}!",
+                    $"Greetings, traveler {userName}!",
+                    $"Behold, the legendary {userName} has arrived!"
+                };
+
+            // Pick a random greeting
+            Random rnd = new Random();
+            int greetingIndex = rnd.Next(greetings.Length); //maxValue is exclusive
+
+            return greetings[greetingIndex];
+        }
+    }
+}
