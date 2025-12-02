36a3ebf0ad936e66fa26440f86b27fb6908fde6b
Maria Pashkulova <132500391+maria-pashkulova@users.noreply.github.com>
2025-12-02 21:50:17 +0200

    Complete task 2

---
+++ SchoolOfAutomationTasks/HW_Session_5_Strings/StringsCompare/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace StringsCompare
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string a = "Test";
+            string b = "test";
+
+            bool areEqual = string.Equals(a, b, StringComparison.OrdinalIgnoreCase);
+            Console.WriteLine($"Strings {a} and {b} are equal (case-insensitive): {areEqual}");
+        }
+    }
+}
