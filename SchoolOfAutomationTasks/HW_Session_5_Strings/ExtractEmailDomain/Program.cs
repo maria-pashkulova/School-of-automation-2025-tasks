9d0535d8310f4a7f1de679c54639677d05988cf3
Maria Pashkulova <132500391+maria-pashkulova@users.noreply.github.com>
2025-12-02 21:45:44 +0200

    Complete task 1

---
+++ SchoolOfAutomationTasks/HW_Session_5_Strings/ExtractEmailDomain/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace ExtractEmailDomain
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter valid email: ");
+            string email = Console.ReadLine();
+
+            string domain = ExtractDomain(email);
+            if (!string.IsNullOrEmpty(domain))
+            {
+                Console.WriteLine($"Email addrress domain: {domain}");
+            }
+            else
+            {
+                Console.WriteLine("Invalid email format.");
+            }
+        }
+        public static string ExtractDomain(string email)
+        {
+            int atIndex = email.IndexOf('@');
+            if (atIndex == -1)
+            {
+                return "";
+            }
+            return email.Substring(atIndex + 1);
+        }
+    }
+}
