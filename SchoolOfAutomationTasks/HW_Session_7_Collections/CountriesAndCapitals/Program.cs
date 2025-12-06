711b983694d19c7bc7903bf8ba43560846e7a266
Maria Pashkulova <132500391+maria-pashkulova@users.noreply.github.com>
2025-12-06 18:17:54 +0200

    Complete tasks 8,9 and 10

---
+++ SchoolOfAutomationTasks/HW_Session_7_Collections/CountriesAndCapitals/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace CountriesAndCapitals
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string, string> capitals = new Dictionary<string, string>();
+            capitals["Bulgaria"] = "Sofia"; // Add more entries
+            capitals["France"] = "Paris";
+            capitals["Germany"] = "Berlin";
+            capitals["Italy"] = "Rome";
+            capitals["Spain"] = "Madrid";
+            capitals["United Kingdom"] = "London";
+
+            Console.WriteLine($"The capital city of Bulgaria is: {capitals["Bulgaria"]}");
+
+            Console.WriteLine("All countries and capitals in the list:");
+            foreach (var entry in capitals)
+            {
+                Console.WriteLine($"{entry.Key} - {entry.Value}");
+            }
+
+        }
+    }
+}
