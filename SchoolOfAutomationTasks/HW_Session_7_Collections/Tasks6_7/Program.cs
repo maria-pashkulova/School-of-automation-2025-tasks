8f543e37a7d40c2d5c7c9b1070993d8ee9aaf506
Maria Pashkulova <132500391+maria-pashkulova@users.noreply.github.com>
2025-12-06 18:06:32 +0200

    Complete tasks 6 and 7

---
+++ SchoolOfAutomationTasks/HW_Session_7_Collections/Tasks6_7/Program.cs
@@ -0,0 +1,33 @@
+﻿namespace Tasks6_7
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> names = new List<string>();
+            names.Add("Ani");
+            names.Add("Borislav");
+            names.Add("Maria");
+            names.Add("Nikola");
+            names.Add("Ivan");
+
+            Console.Write("Initial names in list: ");
+            PrintNames(names);
+
+            names.Remove("Ani");
+
+            Console.Write($"{Environment.NewLine}Updated list: ");
+            PrintNames(names);
+
+
+        }
+
+        static void PrintNames(List<string> names)
+        {
+            foreach (var name in names)
+            {
+                Console.Write($"{name} ");
+            }
+        }
+    }
+}
